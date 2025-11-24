65f9a5ffb33e4b36631a81194f8cedcc5937603a
KakkoiiSaber <kakkoiisaber@gmail.com>
2025-11-23 19:46:50 -0500

    Implement function to clean music file names
    Added a function to remove redundant patterns from music file names.

--- append_bg_music.py
+++ append_bg_music.py
@@ -3,15 +3,22 @@ from pathlib import Path
 import json,time,requests,re
 import toml
 
+def remove_redundant(text: str) -> str:
+    redundant_list = ['.mp3', '_intro', '_loop']
+    for pattern in redundant_list:
+        if pattern in text:
+            text = text.replace(pattern, '')
+    return text
+
 with requests.get('https://raw.githubusercontent.com/KakkoiiSaber/arknights_story_data/main/assets/cn/audio_table.json') as r:
     audio_table = r.json()
 
 music_list = []
 for key, value in audio_table.items():
     if value['intro'] is not None:
-        music_list.append(value['intro'].replace('.mp3', ''))
+        music_list.append(remove_redundant(value['intro']))
     if value['loop'] is not None:
-        music_list.append(value['loop'].replace('.mp3', ''))
+        music_list.append(remove_redundant(value['loop']))
 
 
 with open('config.toml', 'r') as file:
